241491bee20523a8c614a1a5c56c367fe467c582
AHISSOU Florent <AHISSOU Florent@DESKTOP-03MSCPC>
2017-09-20 17:00:32 +0100

    premier depot sur git

---
+++ src/java/util/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author ohonvoh
+ */
+public final class Constants {
+
+    public static final int MONTHSTART = 0;
+    public static final int MONTHEND = 1;
+    public static final int YEARSTART = -1;
+    public static final int YEAREND = -2;
+    public static final int ANNUEL = -3;
+    public static final int MENSUEL = -4;
+    public static final String TYPEFACTURECODE = "ARCEP";
+    public static final int DELAIRELANCE = 14; // en nombre de jours
+
+}
